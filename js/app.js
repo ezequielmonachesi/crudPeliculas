1e0cf352a441213f965c7521adaf08f492231a40
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-04-14 10:35:33 -0300

    agregamos configuracion del tema en localStorage

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
-let btnThemeLight = document.querySelector('#btnThemeLight');
-let btnThemeDark = document.querySelector('#btnThemeDark');
+let btnThemeLight = document.querySelector("#btnThemeLight");
+let btnThemeDark = document.querySelector("#btnThemeDark");
 
-btnThemeLight.addEventListener('click', ()=>cambiarTema('light'));
-btnThemeDark.addEventListener('click', ()=> cambiarTema('dark'));
+btnThemeLight.addEventListener("click", () => cambiarTema("light"));
+btnThemeDark.addEventListener("click", () => cambiarTema("dark"));
 
-function cambiarTema(color){
-    console.log(color)
-    //data-bs-theme = 'light/dark'
-    document.querySelector('html').setAttribute('data-bs-theme', color);
+//leer el localStorage
+let temaConfigurado = JSON.parse(localStorage.getItem('tema')) || 'dark';
+console.log(temaConfigurado)
+cambiarTema(temaConfigurado)
+
+function cambiarTema(color) {
+  console.log(color);
+  //data-bs-theme = 'light/dark'
+  document.querySelector("html").setAttribute("data-bs-theme", color);
+  //guardar en Localstorage
+  localStorage.setItem('tema', JSON.stringify(color));
 }
