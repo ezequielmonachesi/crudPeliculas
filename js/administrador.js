a0eb59d20bf0ce0ed9e4bd320f4f15183c4626a2
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-04-18 10:39:51 -0300

    agregamos prototipo de pelis y creamos un objeto nuevaPeli

---
+++ js/administrador.js
@@ -0,0 +1,5 @@
+import Pelicula from "./classPeliculas.js";
+
+let nuevaPeli = new Pelicula(0,'Super Mario', 'algun desc', '-', 'url de imagen', 'aventura', 2023, 93, 'EEUU', 'varios actores');
+
+console.log(nuevaPeli)
