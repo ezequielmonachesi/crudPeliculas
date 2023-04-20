728ba2bafd75cd146bbb86870c95b4c8176ed9d6
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-04-20 08:51:47 -0300

    crear pelicula avanzamos con local storage

--- js/administrador.js
+++ js/administrador.js
@@ -11,8 +11,14 @@ function prepararFormulario(e){
     crearPelicula();
 }
 
-function crearPelicula(e){
-    let nuevaPeli = new Pelicula('Super Mario', 'algun desc', '-', 'url de imagen', 'aventura', 2023, 93, 'EEUU', 'varios actores');
+function crearPelicula(){
+    //validar el formulario
+    let nuevaPeli = new Pelicula('Super mario','alguna desc','-','url de imagen','aventura', 2023, 93,'EEUU','varios actores');
     console.log(nuevaPeli);
-
+    //guardar la peli en el array
+    listaPeliculas.push(nuevaPeli)
+    console.log(listaPeliculas)
+    //guardar el array en localstorage
+    localStorage.setItem('listaPeliculas', JSON.stringify(listaPeliculas));
+    //limpiar el formulario
 }
